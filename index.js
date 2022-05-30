7ad01358c605722fbd91cbb84629d6b748a663a4
Chang Shin <cshin7374@gmail.com>
2022-05-30 17:03:49 -0500

    commit tree 89ddea33931a41824387f3a7b8127e357670855a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.querySelector('#dodger');
+
+dodger.style.backgroundColor = '#FF69B4';
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10)
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10)
+    if (left <= 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
